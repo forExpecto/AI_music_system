5c36a143fa04b9090a47108d0c0dd2f4ff134a8c
forExpecto <1838844858@qq.com>
2024-10-29 11:41:26 +0800

    登录界面更新

--- eightandhalf/src/router/index.js
+++ eightandhalf/src/router/index.js
@@ -1,10 +1,9 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import HomePart from "@/views/HomePart.vue";
-import MusicPart from "@/views/MusicPart.vue";
-import ArtistPart from "@/views/ArtistPart.vue";
 import LookForMusic from "@/views/LookForMusic.vue";
 import MyMusic from "@/views/MyMusic.vue";
 import MusicCommunity from "@/views/MusicCommunity.vue";
+import LoginView from "@/views/Login/LoginView.vue";
 
 
 const routes = [
@@ -12,15 +11,7 @@ const routes = [
         path: '/',
         component: HomePart
     },
-    {
-        path: '/music',
-        component: MusicPart
-    },
 
-    {
-        path: '/artist',
-        component: ArtistPart
-    },
     {
         path: '/lookForMusic',
         component: LookForMusic
@@ -32,7 +23,11 @@ const routes = [
     {
         path: '/musicCommunity',
         component: MusicCommunity
-    }
+    },
+    {
+        path: '/login',
+        component: LoginView
+    },
 ]
 
 const router = createRouter({
